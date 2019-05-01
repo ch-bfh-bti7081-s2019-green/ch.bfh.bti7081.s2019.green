575c15fd80f4e3c069a5726443e0fd555bd1f4fb
Severin Kaderli <severin@kaderli.dev>
2019-05-01 17:34:51 +0200

    Add javadoc generation to CI
    Signed-off-by: Severin Kaderli <severin@kaderli.dev>

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
-sudo mv "/tmp/ROOT.war" "/opt/tomcat/webapps/ROOT.war"
-sudo chown tomcat:tomcat "/opt/tomcat/webapps/ROOT.war"
-sudo systemctl restart tomcat
+MODE="code"
+
+if [ -n "${1}" ]; then
+    MODE="${1}"
+fi
+
+if [ "${MODE}" == "doc" ]; then
+    sudo mkdir -p "/opt/tomcat/webapps/docs"
+    sudo mv "/tmp/apidocs/*" "/opt/tomcat/webapps/docs"
+    sudo chown -R tomcat:tomcat "/opt/tomcat/webapps/docs"
+    sudo systemctl restart tomcat
+else
+    sudo mv "/tmp/ROOT.war" "/opt/tomcat/webapps/ROOT.war"
+    sudo chown tomcat:tomcat "/opt/tomcat/webapps/ROOT.war"
+    sudo systemctl restart tomcat
+fi
+
