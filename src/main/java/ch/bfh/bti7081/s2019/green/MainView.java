4020cf422d22d5947ff3bed626703bd84f1579ed
Severin Kaderli <severin@kaderli.dev>
2019-05-03 15:33:46 +0200

    Add main layout
    Signed-off-by: Severin Kaderli <severin@kaderli.dev>

--- src/main/java/ch/bfh/bti7081/s2019/green/MainView.java
+++ src/main/java/ch/bfh/bti7081/s2019/green/MainView.java
@@ -1,5 +1,6 @@
 package ch.bfh.bti7081.s2019.green;
 
+import ch.bfh.bti7081.s2019.green.layout.DefaultRouterLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -9,13 +10,13 @@ import com.vaadin.flow.server.PWA;
 /**
  * The main view contains a button and a click listener.
  */
-@Route
+@Route(layout = DefaultRouterLayout.class)
 @PWA(name = "Patient Management System", shortName = "PMS")
 public class MainView extends VerticalLayout {
 
     public MainView() {
         Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
+                event -> Notification.show("You have clicked the 'Click me'-Button!"));
         add(button);
     }
 }
---
+++ src/main/java/ch/bfh/bti7081/s2019/green/layout/DefaultRouterLayout.java
@@ -0,0 +1,49 @@
+package ch.bfh.bti7081.s2019.green.layout;
+
+import ch.bfh.bti7081.s2019.green.MainView;
+import com.github.appreciated.app.layout.behaviour.Behaviour;
+import com.github.appreciated.app.layout.builder.AppLayoutBuilder;
+import com.github.appreciated.app.layout.component.appbar.AppBarBuilder;
+import com.github.appreciated.app.layout.component.menu.left.builder.LeftAppMenuBuilder;
+import com.github.appreciated.app.layout.component.menu.left.items.LeftNavigationItem;
+import com.github.appreciated.app.layout.entity.DefaultBadgeHolder;
+import com.github.appreciated.app.layout.notification.DefaultNotificationHolder;
+import com.github.appreciated.app.layout.notification.component.AppBarNotificationButton;
+import com.github.appreciated.app.layout.notification.entitiy.DefaultNotification;
+import com.github.appreciated.app.layout.router.AppLayoutRouterLayout;
+import com.vaadin.flow.component.icon.VaadinIcon;
+
+import static com.github.appreciated.app.layout.entity.Section.FOOTER;
+import static com.github.appreciated.app.layout.entity.Section.HEADER;
+
+
+public class DefaultRouterLayout extends AppLayoutRouterLayout {
+    /**
+     * Do not initialize here. This will lead to NPEs
+     */
+    private DefaultNotificationHolder notifications;
+    private DefaultBadgeHolder badge;
+
+    public DefaultRouterLayout() {
+        notifications = new DefaultNotificationHolder(newStatus -> {
+        });
+        badge = new DefaultBadgeHolder(5);
+        for (int i = 1; i < 6; i++) {
+            notifications.addNotification(new DefaultNotification("Test title" + i, "A rather long test description ..............." + i));
+        }
+        LeftNavigationItem menuEntry = new LeftNavigationItem("Menu", VaadinIcon.MENU.create(), MainView.class);
+        badge.bind(menuEntry.getBadge());
+        init(AppLayoutBuilder
+                .get(Behaviour.LEFT_RESPONSIVE_HYBRID)
+                .withTitle("Patient Management System")
+                .withAppBar(AppBarBuilder.get()
+                        .add(new AppBarNotificationButton<>(VaadinIcon.BELL, notifications))
+                        .build())
+                .withAppMenu(LeftAppMenuBuilder.get()
+                        .addToSection(new LeftNavigationItem("Mood Diary", VaadinIcon.BOOK.create(), MainView.class), HEADER)
+                        .addToSection(new LeftNavigationItem("Reminders", VaadinIcon.BELL.create(), MainView.class), FOOTER)
+                        .build())
+                .build());
+    }
+}
+
