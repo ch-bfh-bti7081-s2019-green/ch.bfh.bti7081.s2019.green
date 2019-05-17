9ff89268a29d7ae0da844db3151d3e219ad194e0
jmpout <jmpout@riseup.net>
2019-05-17 03:33:25 +0200

    imports and textfields removed

--- src/main/java/ch/bfh/bti7081/s2019/green/view/LoginViewImpl.java
+++ src/main/java/ch/bfh/bti7081/s2019/green/view/LoginViewImpl.java
@@ -1,13 +1,6 @@
 package ch.bfh.bti7081.s2019.green.view;
 
-import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.ComponentEventListener;
-import com.vaadin.flow.component.login.AbstractLogin;
 import com.vaadin.flow.component.login.LoginForm;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.PasswordField;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,9 +8,6 @@ import java.util.List;
 public class LoginViewImpl implements LoginView {
 
     private List<LoginButtonListener> loginButtonListener = new ArrayList<LoginButtonListener>();
-    private TextField usernameField;
-    private PasswordField passwordField;
-
 
     public LoginViewImpl() {
 
