3d1c9b63d6aa09c3c94a6cd57bb00550d193f5bd
Severin Kaderli <severin@kaderli.dev>
2019-05-17 15:14:35 +0200

    Add support for database seeding
    Signed-off-by: Severin Kaderli <severin@kaderli.dev>

---
+++ src/main/java/ch/bfh/bti7081/s2019/green/persistence/seed/DatabaseSeeder.java
@@ -0,0 +1,7 @@
+package ch.bfh.bti7081.s2019.green.persistence.seed;
+
+public class DatabaseSeeder {
+    public static void main(String[] args) {
+        PersonSeeder.seed();
+    }
+}
---
+++ src/main/java/ch/bfh/bti7081/s2019/green/persistence/seed/PersonSeeder.java
@@ -0,0 +1,7 @@
+package ch.bfh.bti7081.s2019.green.persistence.seed;
+
+class PersonSeeder {
+    static void seed() {
+        System.out.println("Include Seeding here");
+    }
+}
