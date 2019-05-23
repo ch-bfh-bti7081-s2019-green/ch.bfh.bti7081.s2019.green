1a68d8511708c1034b641e1ae7fb82f8b86be51c
Severin Kaderli <severin@kaderli.dev>
2019-05-23 13:41:39 +0200

    Clean up code a little bit
    Signed-off-by: Severin Kaderli <severin@kaderli.dev>

--- src/main/java/ch/bfh/bti7081/s2019/green/persistence/seed/DatabaseSeeder.java
+++ src/main/java/ch/bfh/bti7081/s2019/green/persistence/seed/DatabaseSeeder.java
@@ -1,23 +1,33 @@
 package ch.bfh.bti7081.s2019.green.persistence.seed;
 
 import ch.bfh.bti7081.s2019.green.persistence.SessionSingleton;
+import org.hibernate.boot.Metadata;
+import org.hibernate.mapping.PersistentClass;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.hibernate.tool.schema.TargetType;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 
+
+/**
+ * This class is used to nuke the current database, rebuild it and seed it
+ * with predefined data.
+ *
+ * This class can be directly executed using maven:
+ * <code>mvn compile exec:java@seed</code>
+ */
 public class DatabaseSeeder {
 
     private static SessionSingleton db = SessionSingleton.getInstance();
+    private static Metadata metadata = MetadataExtractorIntegrator.INSTANCE.getMetadata();
 
     public static void main(String[] args) {
         nukeDatabase();
         rebuildDatabase();
 
-        //PersonSeeder.seed();
-        //MoodDiarySeeder.seed();
+        // TODO: Add seed code here
     }
 
     /**
@@ -25,14 +35,11 @@ public class DatabaseSeeder {
      * with PostgreSQL.
      */
     private static void nukeDatabase() {
-
         db.executeInTransactionNoResult(session -> {
             session.createNativeQuery(getNukeQuery()).executeUpdate();
             session.clear();
             session.flush();
         });
-
-        System.out.println("Dropping table");
     }
 
     /**
@@ -43,23 +50,43 @@ public class DatabaseSeeder {
         schemaExport.setHaltOnError(true);
         schemaExport.setFormat(true);
         schemaExport.setDelimiter(";");
-        schemaExport.setOutputFile("db-schema.sql");
-        schemaExport.execute(EnumSet.of(TargetType.DATABASE), SchemaExport.Action.CREATE, MetadataExtractorIntegrator.INSTANCE.getMetadata());
+        schemaExport.execute(EnumSet.of(TargetType.DATABASE), SchemaExport.Action.CREATE, metadata);
     }
 
+    /**
+     * Returns a list of all tables registered by in hibernate.
+     *
+     * @return A list of table names
+     */
     private static List<String> getTables() {
-        return Arrays.asList("mood_diaries", "entries");
+        List<String> tables = new ArrayList<>();
+
+        for (PersistentClass persistentClass : metadata.getEntityBindings()) {
+            tables.add(persistentClass.getTable().getName());
+        }
+
+        // TODO: Get names of join tables using the hibernate metadata
+        tables.add("defer_times");
+        tables.add("emergency_contact");
+
+        return tables;
     }
 
+    /**
+     * Return the SQL query to drop all tables in the database. This is
+     * currently only compatible with PostgreSQL.
+     *
+     * @return A string containing the nuke query
+     */
     private static String getNukeQuery() {
         StringBuilder sb = new StringBuilder();
         List<String> tables = getTables();
         for (String table : tables) {
-            sb.append("DROP TABLE IF EXISTS " + table + " CASCADE;");
+            sb.append("DROP TABLE IF EXISTS ");
+            sb.append(table);
+            sb.append(" CASCADE;");
         }
 
         return sb.toString();
     }
-
-
 }
--- src/main/java/ch/bfh/bti7081/s2019/green/persistence/seed/PersonSeeder.java
+++
@@ -1,7 +0,0 @@
-package ch.bfh.bti7081.s2019.green.persistence.seed;
-
-public class PersonSeeder {
-    public static void seed() {
-        System.out.println("Include Seeding here");
-    }
-}
