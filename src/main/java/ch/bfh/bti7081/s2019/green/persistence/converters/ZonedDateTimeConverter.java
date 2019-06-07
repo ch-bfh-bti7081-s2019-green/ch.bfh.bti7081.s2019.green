f8a66126d176e6a6c3388bbde25054c7f5d5623c
Severin Kaderli <severin@kaderli.dev>
2019-06-07 14:47:42 +0200

    Change ZonedDateTimeConverter
    Signed-off-by: Severin Kaderli <severin@kaderli.dev>

--- src/main/java/ch/bfh/bti7081/s2019/green/persistence/converters/ZonedDateTimeConverter.java
+++ src/main/java/ch/bfh/bti7081/s2019/green/persistence/converters/ZonedDateTimeConverter.java
@@ -1,22 +1,24 @@
 package ch.bfh.bti7081.s2019.green.persistence.converters;
 
+import com.vaadin.flow.server.VaadinSession;
+
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
+import java.sql.Timestamp;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Converter(autoApply = true)
-public class ZonedDateTimeConverter implements AttributeConverter<ZonedDateTime, String> {
-
-    private static DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+public class ZonedDateTimeConverter implements AttributeConverter<ZonedDateTime, Timestamp> {
 
     @Override
-    public String convertToDatabaseColumn(ZonedDateTime attribute) {
-        return attribute.format(FORMATTER);
+    public Timestamp convertToDatabaseColumn(ZonedDateTime attribute) {
+        return Timestamp.valueOf(attribute.withZoneSameInstant(ZoneId.of("UTC")).toLocalDateTime());
     }
 
     @Override
-    public ZonedDateTime convertToEntityAttribute(String dbData) {
-        return ZonedDateTime.parse(dbData, FORMATTER);
+    public ZonedDateTime convertToEntityAttribute(Timestamp dbData) {
+        return dbData.toLocalDateTime().atOffset(ZoneOffset.of(VaadinSession.getCurrent().getBrowser().getTimeZoneId())).toZonedDateTime();
     }
 }
